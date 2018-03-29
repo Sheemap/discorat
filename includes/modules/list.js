718eaac6fefb890c36298cb5a6aa12b2a73c6d5e
Sheemap <jamman98@gmail.com>
2018-03-29 00:12:08 -0600

    Add command to list channels in a guild

--- includes/modules/list.js
+++ includes/modules/list.js
@@ -9,7 +9,7 @@ const logger = require('../logger.js');
 exports.usage = ``;
 
 exports.main = function(msg,args){
-	
+
 	switch(args[0].replace(app.prefix,'')){
 
 		case 'list':
@@ -19,8 +19,38 @@ exports.main = function(msg,args){
 		case 'guilds':
 			listGuilds(msg);
 			break;
+
+		case 'channels':
+			listChans(msg,args[1]);
+			break;
 	}
 	
+}
+
+function listChans(msg,chanid){
+	let rawguilds = app.client.guilds.array();
+
+	let content = '';
+
+	for(let i=0;i<rawguilds.length;i++){
+		if(rawguilds[i].id == chanid){
+
+			content += `**Channels I see in ${rawguilds[i].name}**\n\n`
+
+			let rawchans = rawguilds[i].channels.array();
+
+			for(let w=0;w<rawchans.length;w++){11
+				content += `**${rawchans[w].name}** ~ ${rawchans[w].id} ~ **${rawchans[w].type}**\n`;
+			}
+
+		}
+	}
+
+	if(content == ''){
+		common.sendMsg(msg,`No guild found with ID **${chanid}**, please try again.`);
+	}else{
+		common.sendMsg(msg,content)
+	}
 }
 
 function listGuilds(msg){
